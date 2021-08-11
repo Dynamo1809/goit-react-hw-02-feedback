292aba66b1479669d0f76a2c3fe2cd160061c609
Dmytro Maksiuta <d.maksiuta18@gmail.com>
2021-08-12 02:55:28 +0300

    feedback

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,7 +3,15 @@ import styles from './FeedbackOptions.module.scss';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   const optionsKeys = Object.keys(options); 
   return optionsKeys.map( (option, ind) => (
-      <button className={styles.FeedbackOptions__button} value={option} key={option} type="button" onClick={onLeaveFeedback}>{option}</button>
+      <button 
+        className={styles.FeedbackOptions__button}
+        value={option}
+        key={option}
+        type="button" 
+        onClick={onLeaveFeedback}
+      >
+        {option}
+      </button>
     )
   )  
 };
